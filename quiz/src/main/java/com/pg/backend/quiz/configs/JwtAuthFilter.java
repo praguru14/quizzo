06ff96a6d4b9262b070e82b60f76af3c39965ca3
prafull <praguru14@gmail.com>
2022-02-20 22:07:34 +0530

    Changing/Adding SecurityConfig.java, JwtUtils

---
+++ quiz/src/main/java/com/pg/backend/quiz/configs/JwtAuthFilter.java
@@ -0,0 +1,26 @@
+package com.pg.backend.quiz.configs;
+
+import com.pg.backend.quiz.services.UserDetailsSecurityService;
+import com.pg.backend.quiz.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsSecurityService userDetailsSecurityService;
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
--- quiz/src/main/java/com/pg/backend/quiz/configs/SecurityConfig.java
+++ quiz/src/main/java/com/pg/backend/quiz/configs/SecurityConfig.java
@@ -4,12 +4,15 @@ import com.pg.backend.quiz.services.UserDetailsSecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,17 +25,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+
+    @Autowired
     private UserDetailsSecurityService userDetailsSecurityService;
 
     //How to do configuration
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsSecurityService).passwordEncoder(passwordEncoder());
-        super.configure(auth);
     }
 
+    //which end point to use and public them
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf()
+            .disable().cors().disable().authorizeRequests().
+            antMatchers("generate-token","user").permitAll()
+                        .antMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                        .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+                        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         super.configure(http);
     }
 }
