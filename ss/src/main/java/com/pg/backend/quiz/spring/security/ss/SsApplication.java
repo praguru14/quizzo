db85dafb9fc56eab54b89045568994062a09d20d
prafull <praguru14@gmail.com>
2022-02-21 09:24:36 +0530

    Adding SS refresher for self practice

---
+++ ss/src/main/java/com/pg/backend/quiz/spring/security/ss/SsApplication.java
@@ -0,0 +1,13 @@
+package com.pg.backend.quiz.spring.security.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsApplication.class, args);
+	}
+
+}
