51ad306651e929e381639adb4b6058f186818030
prafull <praguru14@gmail.com>
2022-02-25 10:20:55 +0530

    In Memory Security

---
+++ ss/src/main/java/com/pg/backend/quiz/spring/security/ss/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.pg.backend.quiz.spring.security.ss.config;
+
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("john").password("john").roles("USER");
+        auth.inMemoryAuthentication().withUser("pg").password("pg").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ ss/src/main/java/com/pg/backend/quiz/spring/security/ss/config/SwaggerConfig.java
@@ -0,0 +1,7 @@
+package com.pg.backend.quiz.spring.security.ss.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+}
